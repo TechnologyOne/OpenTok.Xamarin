85f5a42ca3257337f70c27f52e1d0ed7acb3eb0c
PlymouthRock <tim@techone.us.com>
2014-12-29 16:44:27 -0500

    Updated Linker Settings

--- OpenTok.Xamarin.Binding/OpenTok.linkwith.cs
+++ OpenTok.Xamarin.Binding/OpenTok.linkwith.cs
@@ -1,4 +1,5 @@
 ﻿using System;
 using MonoTouch.ObjCRuntime;
-
+ 
+// Linker Settings
 [assembly: LinkWith ("OpenTok", LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "CoreTelephony MobileCoreServices CFNetwork SystemConfiguration CoreMedia Security AudioToolbox CoreAudio CoreVideo OpenGLES QuartzCore AVFoundation CoreGraphics", LinkerFlags = "-lstdc++.6.0.9 -lsqlite3 -ObjC", IsCxx = true, SmartLink = true, ForceLoad = true)]
