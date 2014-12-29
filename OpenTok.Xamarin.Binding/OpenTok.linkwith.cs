dcc69e9551ac78e07e8f07d3e7628a03e8be20a1
PlymouthRock <tim@techone.us.com>
2014-12-29 16:25:29 -0500

    Updates to Linkwith

--- OpenTok.Xamarin.Binding/OpenTok.linkwith.cs
+++ OpenTok.Xamarin.Binding/OpenTok.linkwith.cs
@@ -1,5 +1,4 @@
 ﻿using System;
 using MonoTouch.ObjCRuntime;
 
-//[assembly: LinkWith ("OpenTok", LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "CoreTelephony MobileCoreServices CFNetwork SystemConfiguration CoreMedia Security AudioToolbox CoreAudio CoreVideo OpenGLES QuartzCore AVFoundation CoreGraphics", LinkerFlags = "-lstdc++ -lz -ObjC", IsCxx = true, SmartLink = true, ForceLoad = true)]
 [assembly: LinkWith ("OpenTok", LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "CoreTelephony MobileCoreServices CFNetwork SystemConfiguration CoreMedia Security AudioToolbox CoreAudio CoreVideo OpenGLES QuartzCore AVFoundation CoreGraphics", LinkerFlags = "-lstdc++ -lstdc++.6.0.9 -lpthread -lsqlite3 -lxml2 -ObjC", IsCxx = true, SmartLink = true, ForceLoad = true)]
